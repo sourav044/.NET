4a32b6abcb8ce301983ea3ea75d2c4b91a1e15c0
Sourav Kumar <sourav044@hotmail.com>
2019-03-01 23:02:12 +0100

    Focus - Login & MVVM using Prism

---
+++ MVVM - Form Validation/MVVM - Form Validation/ViewModels/EMP_OLD_ViewModel.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace MVVM___Form_Validation.ViewModels
+{
+    class EMP_OLD_ViewModel : INotifyPropertyChanged
+    {
+        private string _name;
+
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                Name = value;
+                OnPropertyChanged();
+            }
+        }
+
+      
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged([CallerMemberName] string property = "")
+        {
+
+            var handler = PropertyChanged; //Sometime it will lose while exexuting 
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(property));
+            }
+
+        }
+    }
+
+
+    class MyCommand : ICommand
+    {
+
+        public MyCommand(EMP_OLD_ViewModel emp)
+        {
+
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Execute(object parameter)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ MVVM - Form Validation/MVVM - Form Validation/ViewModels/EmployeeViewModel.cs
@@ -0,0 +1,105 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace MVVM___Form_Validation.ViewModels
+{
+    class EmployeeViewModel : BindableBase
+    {
+        #region prop set
+
+        private string _name;
+
+        public string Name
+        {
+            get { return _name; }
+            set { SetProperty(ref _name, value); }
+        }
+
+        private string _email;
+
+        public string Email
+        {
+            get { return _email; }
+            set { SetProperty(ref _email, value); }
+        }
+
+        private int _mobile;
+
+        public int Mobile
+        {
+            get { return _mobile; }
+            set { SetProperty(ref _mobile, value); }
+        }
+
+        private double _salray;
+
+        public double Salary
+        {
+            get { return _salray; }
+            set { SetProperty(ref _salray, value); }
+        }
+
+
+        #endregion
+
+        public DelegateCommand Employee_button { get; set; }
+
+
+        public EmployeeViewModel()
+        {
+            Employee_button = new DelegateCommand(Exceute, CanExecute).ObservesProperty(() => Name).ObservesProperty(() => Mobile).ObservesProperty(() => Email);
+        }
+
+        private bool CanExecute()
+        {
+            bool _rval = false;
+            
+            if (String.IsNullOrEmpty(Name))
+            {
+                 return false;
+            }
+            
+            if(!string.IsNullOrEmpty(Email) && IsValidEmail(Email) == false)
+            {
+                MessageBox.Show("Please enter valid mail.");
+                return _rval = false;                
+            }
+
+            if (Mobile.ToString().Trim() == "0" )
+            {
+                return _rval = false;
+            }
+
+
+            return _rval  = true;
+        }
+
+        private void Exceute()
+        {
+
+            MessageBox.Show("Verified all Fields.");
+
+        }
+
+        //email validation 
+        public bool IsValidEmail(string email)
+        {
+            try
+            {
+                var addr = new System.Net.Mail.MailAddress(email);
+                return addr.Address == email;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+    }
+}
